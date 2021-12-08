51aa91a9d5a63c314328eaa45bf54d1c1415ceff
frylemon <frylemon@yandex.ru>
2021-12-08 13:45:21 +0300

    Правка #2
    Добавлены:
    - Сбор наименований с каждой позиции
    
    Проблема:
    - Капча

--- Сам_Парсер/Main.py
+++ Сам_Парсер/Main.py
@@ -3,29 +3,34 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 webdriver = Service(r'A:\Код\Parser\Сам_Парсер\chromedriver.exe')
 driver = Chrome(service=webdriver)
 
-pages = 10
+pages = 2
 
+wait = WebDriverWait(driver, 10)
 
 for page in range(1, pages):
-
     url = 'https://www.ozon.ru/category/konfety-30695/?page=' + str(page)
     driver.get(url)
+    wait.until(lambda driver: driver.find_element(By.TAG_NAME, 'h1').text == 'Конфеты')
 
-    candies = driver.find_elements(By.CLASS_NAME, 'tile-hover-target')
+    candies_link = [0]*36
+    for p in range(1,37):
+        candies_link[p - 1] = driver.find_elements(By.XPATH, '/html/body/div[1]/div/div['
+                                                '1]/div[3]/div[2]/div[2]/div[3]/div[1]'
+                                                           '/div/div/div[' + str(p) + ']/a')[0].get_attribute('href')
 
-    for candy in candies:
-        candy.click()
-
-        wait = WebDriverWait(driver, 10)
+    for candy_link in candies_link:
+        driver.get(candy_link)
         wait.until(lambda driver: driver.find_element(By.TAG_NAME, 'h1').text != 'Конфеты')
 
         print(driver.find_element(By.TAG_NAME, 'h1').text)
+
         driver.get(url)
         wait.until(lambda driver: driver.find_element(By.TAG_NAME, 'h1').text == 'Конфеты')
-        break
+    break
 
 driver.quit()
